77f2ef927785f0bdf05ae755282bc6eb4c074553
Vishnu R <vishnu6364748848@gmail.com>
2026-02-27 12:33:37 +0530

    Update friend status toggle in card click event

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 let cards = document.querySelectorAll(".card");
-let isFriends = false;
 cards.forEach((card) => {
-    console.log();
     card.childNodes[5].addEventListener("click", () => {
-        if (!isFriends) {
-            isFriends = true;
+        if (card.childNodes[3].childNodes[5].textContent === "Stranger") {
             card.childNodes[3].childNodes[5].textContent = "friends";
-
             card.childNodes[5].classList.add("following");
             card.childNodes[5].textContent = "Following";
         } else {
-            isFriends = false;
             card.childNodes[3].childNodes[5].textContent = "Stranger";
             card.childNodes[5].classList.remove("following");
             card.childNodes[5].textContent = "Follow";
